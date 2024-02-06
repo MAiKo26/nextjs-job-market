9ee4eb34d4727cc1ab819e1effe475a717108ecf
Mohamed Aziz Karoui <133112004+MAiKo26@users.noreply.github.com>
2024-02-06 11:48:49 +0100

    Create error.tsx

---
+++ src/app/error.tsx
@@ -0,0 +1,14 @@
+"use client";
+interface errorProps {}
+
+function error({}: errorProps) {
+  return (
+    <main className="m-auto my-10 max-w-5xl space-y-5 px-3 text-center">
+      <h1 className="text-4xl font-extrabold tracking-tight lg:text-5xl ">
+        Error
+      </h1>
+      <p> An Unexpexted Error occurred</p>
+    </main>
+  );
+}
+export default error;
