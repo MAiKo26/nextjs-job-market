162a2bbf2058e8786d7f3a927a6cac845d250c37
Mohamed Aziz Karoui <133112004+MAiKo26@users.noreply.github.com>
2024-02-09 12:30:13 +0100

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,14 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Market Job",
-  description: "Nextjs Market Job",
+  title: { default: "Market Job", template: "Market Job | %s" },
+  description: "Find your job inside our Market Job",
 };
 
 export default function RootLayout({
@@ -16,7 +18,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${inter.className} min-w-[350px] `}>{children}</body>
+      <body className={`${inter.className} min-w-[350px] `}>
+        <Navbar />
+        {children}
+        <Footer />
+      </body>
     </html>
   );
 }
